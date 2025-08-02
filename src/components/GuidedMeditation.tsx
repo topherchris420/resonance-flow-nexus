d1f062b45ad11fbf49e7d40ef3b883abff4cb203
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2025-08-02 15:38:53 -0400

    Update GuidedMeditation.tsx

--- src/components/GuidedMeditation.tsx
+++ src/components/GuidedMeditation.tsx
@@ -3,15 +3,15 @@ import React, { useState, useRef } from 'react';
 const meditationTracks = [
   {
     title: 'Mantra for Inner Peace',
-    src: 'public/audio/medi.mp3',
+    src: '/audio/medi.mp3',
   },
   {
     title: 'Affirmations for Clarity',
-    src: 'public/audio/medi.mp3',
+    src: '/audio/medi.mp3',
   },
   {
     title: '20-Minute Guided SRV Session',
-    src: 'public/audio/medi.mp3',
+    src: '/audio/medi.mp3',
   },
 ];
 
